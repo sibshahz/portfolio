d731c2a9773260a1e3a616cf8111a6789734b235
Shahid Gillani <shahid5ssg@gmail.com>
2022-05-09 23:04:24 +0500

    Added bootstrap icons, adjusted icons animation, gradient and font size of text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,29 @@
 const robot = document.querySelector('.robots .robot');
-const mouse={
-    x:undefined,
-    y:undefined
-}
+const socialIcons = document.querySelectorAll('.social-icons a');
+
+
 window.addEventListener("load",function(){
     gsap.from(robot,{width:650,height:650,autoAlpha:0,duration:1});
+    gsap.fromTo(robot,{fontSize:"5rem"},{color:"purple",fontSize:"5.4rem", duration:0.8,repeat:-1});
+    
+    rotateIcons();
 });
 window.addEventListener("mousemove", function(event) {
     // event.preventDefault();
     moveRobots(event.pageX,event.pageY);
 });
 
+  function rotateIcons(){
+    let TL=gsap.timeline();
+    TL.from(socialIcons,{autoAlpha:0,y:"120", duration:1,stagger:0.6})
+    .fromTo(socialIcons,{rotation:"180"},{rotation:"0",duration:1.8,ease: "elastic.out(1, 0.3)",stagger:0.6},'-=2');
+
+    // gsap.fromTo(socialIcons,{autoAlpha:1,rotation:"180"},{autoAplha:0,rotation:"0",duration:2,ease: "elastic.out(1, 0.3)",stagger:0.4});
+  }
+
   function moveRobots(x,y){
 
-    gsap.to(robot, {x: x+"px",y:y+"px",ease:"elastic.out(0.3, 0.1)", duration: 1.8,delay:0.2})
+    gsap.to(robot, {x: x+"px",y:y+"px",ease:"power4.out",delay:0.4})
 
 
     
